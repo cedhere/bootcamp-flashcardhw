286d4c9a990ba2f47310e92ab96d83d9b43ff138
Cedric Pierre-Louis <cpierrelouis1114@gmail.com>
2024-10-21 11:46:53 -0400

    Add JavaScript functionality

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const flashcards = [
 ];
 
 // You can use flashcards.length to get the length of the array
+let flashcard = document.getElementById('flashcard');
+let flashcardContent = document.getElementById('card-content');
 
 // These two variables will come in handy
 let currentIndex = 0;
@@ -12,10 +14,57 @@ let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
-
+    if (showingTerm){
+        flashcardContent.innerHTML = flashcards[currentIndex].term;
+    } else {
+        flashcardContent.innerHTML = flashcards[currentIndex].definition;
+    }
 }
 
 // The rest of the code you will write is apart of event listeners
+let nextBtn = document.getElementById('next-btn');
+let prevBtn = document.getElementById('prev-btn');
+let addBtn = document.getElementById('add-card-btn');
+
+
+// event listener for the next button
+nextBtn.addEventListener('click', function () {
+    if (currentIndex === flashcards.length - 1){
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+    displayCard();
+});
+
+// event listener for the previous button
+prevBtn.addEventListener('click', function () {
+    if (currentIndex === 0){
+        currentIndex = flashcards.length - 1;
+    } else {
+        currentIndex--;
+    }
+    displayCard();
+});
+
+// event listener for flipping the flashcard
+flashcard.addEventListener('click', function () {
+    if (showingTerm){
+        showingTerm = false;
+    } else {
+        showingTerm = true;
+    }
+    displayCard();
+});
+
+// event listener for adding a flashcard
+addBtn.addEventListener('click', function () {
+    let term = document.getElementById('new-term');
+    let definition = document.getElementById('new-definition');
+
+    flashcards.push({term: term.value, definition: definition.value});
+});
+
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
